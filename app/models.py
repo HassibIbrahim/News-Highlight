48596d426b77ed7538561e92d0c824a2d28bbb2c
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-11-10 21:18:23 +0300

    added news class model for initialization

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class News:
+    '''
+    News class to define News sources Objects
+    '''
+
+    def __init__(self, id, name,url,category):
+        self.id = id
+        self.name = name
+        self.url = url 
+        self.category = category
+
+        
